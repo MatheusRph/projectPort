4f81b99792ef2890d6d4c566c129893434d70b88
MatheusRph <matheus.rodolpho16@gmail.com>
2024-12-22 17:34:32 -0300

    Update the image and remove image from bootstrap for image from next

--- components/MyImage.js
+++ components/MyImage.js
@@ -1,12 +1,12 @@
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
+import Image from 'next/image';
 
 function MyImage() {
   return (
     <Container>
         <Col xs={6} md={4} className='container'>
-          <Image src="image.png" className='myimage' roundedCircle />
+          <Image src="/image.jpg" alt='PROFILE IMAGE' width={325} height={400} className='myimage rounded'/>
         </Col>
     </Container>
   );
